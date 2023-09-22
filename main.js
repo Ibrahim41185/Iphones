050503b1c8545c30e8380b5203300c1e8253d2f9
ibrahim <himaovic1@gamil.com>
2023-09-22 19:59:24 +0300

    update colors

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 
 let img = document.querySelector(".abdo");
+let hima =document.querySelector('.container');
 
  function hello(phons){
     img.src = phons;
 }
+
+
+function colors(colors){
+    hima.style.background = colors;
+}
+
